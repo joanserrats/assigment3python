b0b38443b8284fe44dfb27084c9e756212b368d6
joanserrats <joanserratsmollevi@gmail.com>
2024-01-14 16:28:40 +0100

    creamos la class "film"

--- Scripts/scriptafterclass3.py
+++ Scripts/scriptafterclass3.py
@@ -5,6 +5,16 @@ Script to filter information about dataset film
 import pandas as pd
 import click
 
+
+class Film:
+    def __iniciar__(self, title, year, tickets_sold):        #definimos una clase llamada "film" para poder crear objetos (titulo, año, ventas)
+        self.title = title
+        self.year = year
+        self.tickets_sold = tickets_sold
+
+
+
+
 @click.command()
 @click.option('--title', prompt='Enter film title', help='The title of the film')
 @click.option('--year', prompt='Enter release year', type=int, help='The release year of the film')
